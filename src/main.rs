6cbcdba1bf3c0ce2486d9a90a93612c63d6e0fc2
eddie <appositum@protonmail.com>
2018-11-01 00:54:36 -0300

    Pong ms time

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@
 #[macro_use] extern crate log;
 extern crate env_logger;
 extern crate rand;
+extern crate chrono;
 
 use std::env;
 use rand::Rng;
+use chrono::offset::Utc;
 use serenity::client::Client;
 use serenity::framework::standard::StandardFramework;
 use serenity::model::event::ResumedEvent;
@@ -45,18 +47,32 @@ fn main() {
         .expect("Error creating client");
 
     client.with_framework(StandardFramework::new()
-        .configure(|c| c.prefix("."))
+        .configure(|c| c
+            .prefix(".")
+            .on_mention(true)
+        )
         .cmd("ping", ping)
         .cmd("saydel", saydel)
-        .cmd("oh no", ohno));
+        .cmd("say", saydel)
+        .cmd("oh no", ohno)
+        .cmd("ohno", ohno));
 
     if let Err(reason) = client.start() {
-        error!("An error occurred while running the client: {:?}", reason)
+        error!("An error occurred while running the client: {:#?}", reason)
     }
 }
 
 command!(ping(_ctx, message) {
-    let _ = message.reply("Pong!");
+    let old = Utc::now();
+
+    if let Ok(mut msg) = message.channel_id.say("Pong!") {
+        let new = Utc::now() - old;
+        let _ = msg.edit(|m|
+            m.content(
+                format!("Pong! :ping_pong: took **{} ms**", new.num_milliseconds())
+            )
+        );
+    }
 });
 
 command!(saydel(_ctx, message, args) {
